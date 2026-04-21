42b2fc3c3de505aac55757d4566aaa1da3b35a35
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:17:57 +0300

    5-high-order-functions.js

--- solutions/5-high-order-functions.js
+++ solutions/5-high-order-functions.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
-
-// BEGIN
-
-// END
+const takeOldest = (users, count = 1) => {
+  // Сортируем пользователей по дате рождения (от старых к молодым)
+  const sortedUsers = _.sortBy(users, (user) => Date.parse(user.birthday));
+  // Возвращаем первых count пользователей
+  return _.slice(sortedUsers, 0, count);
+};
+export default takeOldest;
