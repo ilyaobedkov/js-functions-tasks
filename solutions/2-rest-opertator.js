21f786ab82671e4310c528e7191a65ed924a1d83
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:16:02 +0300

    2-rest-opertator.js

--- solutions/2-rest-opertator.js
+++ solutions/2-rest-opertator.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
-
-// BEGIN
-
-// END
+// Вычисление среднего арифметического
+const average = (...args) => {
+  if (args.length === 0) return null;
+  const sum = _.sum(args);
+  return sum / args.length;
+};
+export default average;
