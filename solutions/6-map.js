5b559d198aa24adfa0cf653a0bb96ee786d61a4b
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:18:23 +0300

    6-map.js

--- solutions/6-map.js
+++ solutions/6-map.js
@@ -1,3 +1,7 @@
-// BEGIN
-
-// END
+const getChildren = (users) => {
+  // Извлекаем массив детей для каждого пользователя
+  const childrenArrays = users.map(user => user.children);
+  // Превращаем массив массивов в плоский список
+  return childrenArrays.flat();
+};
+export default getChildren;
