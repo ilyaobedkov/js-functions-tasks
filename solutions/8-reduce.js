5acb80cf19aa4abb59607a8c7e4422982548dcdc
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:19:25 +0300

    8-reduce.js

--- solutions/8-reduce.js
+++ solutions/8-reduce.js
@@ -1,3 +1,14 @@
-// BEGIN
-
-// END
+const groupBy = (objects, property) => {
+  return objects.reduce((acc, obj) => {
+    // Получаем значение свойства, по которому группируем
+    const key = obj[property];
+    // Если такой группы ещё нет, создаём пустой массив
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    // Добавляем текущий объект в соответствующую группу
+    acc[key].push(obj);
+    return acc;
+  }, {});
+};
+export default groupBy;
