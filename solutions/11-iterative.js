4960c90e54bc5795336784581ad3b352459a58a0
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:20:56 +0300

    11-iterative.js

--- solutions/11-iterative.js
+++ solutions/11-iterative.js
@@ -1,8 +1,23 @@
 const smallestDivisor = (num) => {
-    // BEGIN
-
-    // END
+  // Если число равно 1, возвращаем 1
+  if (num === 1) {
+    return 1;
+  }
+  // Начинаем проверку с делителя 2
+  const iter = (divisor) => {
+    // Если делитель больше квадратного корня из числа,
+    // то число простое, возвращаем само число
+    if (divisor * divisor > num) {
+      return num;
+    }
+    // Если число делится на делитель без остатка,
+    // то это наименьший делитель
+    if (num % divisor === 0) {
+      return divisor;
+    }
+    // Иначе проверяем следующий делитель
+    return iter(divisor + 1);
   };
-  
+  return iter(2);
+};
 export default smallestDivisor;
-  
