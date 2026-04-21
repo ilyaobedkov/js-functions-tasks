c1e49a29aa883ad8114617b35c8717fbeb3529fe
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:20:31 +0300

    10-recursion.js

--- solutions/10-recursion.js
+++ solutions/10-recursion.js
@@ -1,8 +1,13 @@
 const sequenceSum = (begin, end) => {
-    // BEGIN
-
-    // END
-  };
-  
+  // Если begin > end, возвращаем NaN (пустая последовательность)
+  if (begin > end) {
+    return NaN;
+  }
+  // Если begin === end, возвращаем это число
+  if (begin === end) {
+    return begin;
+  }
+  // Рекурсивный случай: begin + сумма чисел от begin+1 до end
+  return begin + sequenceSum(begin + 1, end);
+};
 export default sequenceSum;
-  
