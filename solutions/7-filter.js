64f6dd211665615dedbcbb9708ed02f20ecf81cd
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:18:55 +0300

    7-filter.js

--- solutions/7-filter.js
+++ solutions/7-filter.js
@@ -1,3 +1,9 @@
-// BEGIN
-
-// END
+const getGirlFriends = (users) => {
+  // Извлекаем массив друзей для каждого пользователя
+  const friendsArrays = users.map(user => user.friends);
+  // Превращаем массив массивов в плоский список
+  const allFriends = friendsArrays.flat();
+  // Фильтруем только друзей с полом 'female'
+  return allFriends.filter(friend => friend.gender === 'female');
+};
+export default getGirlFriends;
