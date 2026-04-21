6cbf4d61415a5bbf222cd09dbcc50c10e77494f4
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:19:58 +0300

    9-signals.js

--- solutions/9-signals.js
+++ solutions/9-signals.js
@@ -1,12 +1,20 @@
 import get from 'lodash/get.js';
-
 const freeEmailDomains = [
   'gmail.com',
   'yandex.ru',
   'hotmail.com',
   'yahoo.com',
 ];
-
-// BEGIN
-
-// END
+const getFreeDomainsCount = (emails) => {
+  // Извлекаем домены из email'ов
+  const domains = emails.map(email => email.split('@')[1]);
+  // Фильтруем только бесплатные домены
+  const freeDomains = domains.filter(domain => freeEmailDomains.includes(domain));
+  // Подсчитываем количество каждого домена
+  const result = {};
+  for (const domain of freeDomains) {
+    result[domain] = (result[domain] || 0) + 1;
+  }
+  return result;
+};
+export default getFreeDomainsCount;
