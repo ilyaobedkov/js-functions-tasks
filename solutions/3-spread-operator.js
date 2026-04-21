1e0bd3158a5a5f8d55d48e56f9fb7b15e6e22983
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:16:40 +0300

    3-spread-operator.js

--- solutions/3-spread-operator.js
+++ solutions/3-spread-operator.js
@@ -1,3 +1,7 @@
-// BEGIN
-
-// END
+// Конвертации массивов дат в читаемые строки
+const convert = (...dates) => {
+  return dates.map(([year, month, day]) => 
+    new Date(year, month, day).toDateString()
+  );
+};
+export default convert;
