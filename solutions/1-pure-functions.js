14575db766169da2e91372c4d6649e5755ddc8be
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-21 19:15:20 +0300

    1-pure-functions.js

--- solutions/1-pure-functions.js
+++ solutions/1-pure-functions.js
@@ -1,3 +1,13 @@
-// BEGIN
-
-// END
+// Проверяет, является ли число простым
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+// Вывод результата
+const sayPrimeOrNot = (num) => {
+  console.log(isPrime(num) ? 'yes' : 'no');
+};
+export default sayPrimeOrNot;
